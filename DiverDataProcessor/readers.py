9ed4ea8c9055ce286e1ea17e60fbfe43659827d7
Daan Rooze <daan.rooze@deltares.nl>
2025-10-02 11:02:37 +0200

    Added fetch_pressure from NOAA

--- DiverDataProcessor/readers.py
+++ DiverDataProcessor/readers.py
@@ -9,9 +9,9 @@ def read_td_diver(filepath) -> Timeseries:
         filepath,
         usecols=[0, 1, 2],
         names=["date", "diver_pressure (cmH2O)", "temperature (degC)"],
-        decimal=",",
+        decimal=".",
         skiprows=52,
-        delimiter=";",
+        delimiter=",",
         encoding="ISO-8859-1",
         engine="python",
     )
@@ -101,3 +101,45 @@ def read_precipitation(path) -> Timeseries:
     precipitation["date"] = pd.to_datetime(precipitation["date"], format="%d-%m-%Y")
     precipitation = precipitation.set_index("date")
     return Timeseries(precipitation)
+
+
+def fetch_air_pressure(station, start_date, end_date):
+    """
+    Fetches and processes air pressure data from NOAA's Global Hourly dataset.
+
+    Parameters:
+    - station (str): The station ID for which the data is fetched.
+    - start_date (str): The start date in the format 'YYYY-MM-DD'.
+    - end_date (str): The end date in the format 'YYYY-MM-DD'.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing processed air pressure data.
+    """
+    url = (
+        f"https://www.ncei.noaa.gov/access/services/data/v1?"
+        f"dataset=global-hourly&dataTypes=SLP&stations={station}"
+        f"&startDate={start_date}&endDate={end_date}&format=json"
+    )
+    scaling_factor = 10
+    nodata_value = 99999
+    report_type = "FM-15"
+    hours_to_ct = 6
+
+    all_data = pd.read_json(url)
+    selected_data = all_data[all_data["REPORT_TYPE"] == report_type].copy()
+    selected_data["SLP"] = (
+        selected_data["SLP"].replace({",": "."}, regex=True).astype(float) * 0.01
+    )
+    selected_data["date"] = pd.to_datetime(selected_data["DATE"]) - pd.Timedelta(
+        hours=hours_to_ct
+    )
+    selected_data["SLP"] = (
+        selected_data["SLP"].where(selected_data["SLP"] < nodata_value).interpolate()
+        / scaling_factor
+    )
+    output = (
+        selected_data[["date", "SLP"]]
+        .rename(columns={"SLP": "air_pressure (mH2O)"})
+        .set_index("date")
+    )
+    return Timeseries(output)
